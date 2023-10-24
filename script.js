da76d9019ae713e2f2e706c0e1b166facb21aa49
ankit9650 <ankitprabhaker9650@gmail.com>
2023-10-24 13:09:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let string = "";
+let buttons = document.querySelectorAll('.bttn');
+Array.from(buttons).forEach((bttn)=>{
+  bttn.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'AC'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+
+    else if(e.target.innerHTML == 'C'){
+      string = string.substring(0,string.length-1)
+      document.querySelector('input').value = string;
+    }
+
+
+
+
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
